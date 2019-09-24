65c9cc5b415cdb20369e454891530e99754c71d5
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-23 23:04:38 -0300

    added Footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const footer = props =>(
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2019
+            <a href="http://ricardovasconcelos.github.io/Portifolio" target="_blank"> Ricardo Vasconcelos</a>
+        </strong>
+    </footer>
+)
+
+export default footer
--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -3,11 +3,16 @@ import '../common/template/dependencies'
 
 import Header from '../common/header'
 import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer'
 const App = props => {
   return (
     <div className="wrapper">
       <Header/>
       <Sidebar/>
+      <div className="content-wrapper">
+        <h1>Dashboard</h1>
+      </div>
+      <Footer/>
     </div>
   );
 }
