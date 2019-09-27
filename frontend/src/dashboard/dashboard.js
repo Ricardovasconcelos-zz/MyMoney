7c605e70d200fa30dca0e691c825ddbda74efa38
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-27 18:01:12 -0300

    added reducers redux

--- frontend/src/dashboard/dashboard.js
+++ frontend/src/dashboard/dashboard.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import {connect} from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -8,15 +9,17 @@ class Dashboard extends Component{
 
     render(){
 
+        const {credit, debt} = this.props.summary
+
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
-                    <Box cols='12 4' color='green' icon='bank' value='R$ 10' 
+                    <Box cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} 
                     text='Total de Créditos'/>
-                    <Box cols='12 4' color='red' icon='credit-card' value='R$ 10' 
+                    <Box cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} 
                     text='Total de Débitos' />
-                    <Box cols='12 4' color='blue' icon='money' value='R$ 0' 
+                    <Box cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} 
                     text="Valor Consolidado"/>
                 </Content>
             </div>
@@ -24,4 +27,9 @@ class Dashboard extends Component{
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({
+  summary: state.dashboard.summary
+});
+
+
+export default connect(mapStateToProps)(Dashboard)
