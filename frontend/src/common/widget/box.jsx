082ab02590b5757bcda0d081a6313e69ebabaa05
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-26 21:06:28 -0300

    added box dashboard

---
+++ frontend/src/common/widget/box.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Grid from '../layout/Grid'
+
+
+const box = props =>(
+    <Grid>
+        <div className={`small-box bg-${props.color}`}>
+            <div className="inner">
+                <h3>{props.value}</h3>
+                <p>{props.text}</p>
+            </div>
+            <div className="icon">
+                <i className={`fa fa-${props.icon}`}></i>
+            </div>
+        </div>
+    </Grid>
+)
+
+export default box
