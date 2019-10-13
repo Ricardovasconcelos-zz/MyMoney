2de025f38f6e93c7997d876bab7fed72e03cb19d
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-12 22:36:07 -0300

    added operator to verify visible or not

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -4,3 +4,11 @@ export function selectTab(tableId){
         payload: tableId
     }
 }
+export function showTabs(...tabIds){
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return{
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
