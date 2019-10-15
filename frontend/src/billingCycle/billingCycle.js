c9813640b1f91c9313f739f9b0f119379f11e371
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-14 23:07:22 -0300

    added form component using redux form

--- frontend/src/billingCycle/billingCycle.js
+++ frontend/src/billingCycle/billingCycle.js
@@ -11,7 +11,9 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent.jsx'
 import { selectTab, showTabs } from '../common/tab/tabActions'
-import List from '../billingCycle/billingCycleList'
+import { create } from './billingCycleAction'
+import List from './billingCycleList'
+import Form from './billingCycleForm'
 
 class BillingCycle extends Component {
  
@@ -36,7 +38,9 @@ class BillingCycle extends Component {
                             <TabContent id='tabList'>
                                 <List/>
                             </TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}/>
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
@@ -47,6 +51,6 @@ class BillingCycle extends Component {
     }
 }
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({selectTab, showTabs}, dispatch);
+  bindActionCreators({create, selectTab, showTabs}, dispatch);
 
 export default connect(null,mapDispatchToProps)(BillingCycle)
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducers from '../common/tab/tabReducers'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
+
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducers,
-    billingCycle: BillingCycleReducer
+    billingCycle: BillingCycleReducer,
+    form: formReducer
 })
 
 export default rootReducer
