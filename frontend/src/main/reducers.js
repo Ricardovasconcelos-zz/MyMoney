e44161d99f028b9f53dc5cc3a9368aeb238fdea0
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-14 22:42:48 -0300

    added list component and update billingcycle component

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducers from '../common/tab/tabReducers'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducers
+    tab: TabReducers,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
