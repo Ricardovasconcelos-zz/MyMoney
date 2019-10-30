639ed4a2d570023dad5a0a21c8a4f0bb1fb6469d
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-29 22:27:57 -0300

    update billingCycleForm

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,11 +5,20 @@ import { init } from "./billingCycleAction";
 import { reduxForm, Field, formValueSelector } from "redux-form";
 import LabelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component {
+  calculateSummary() {
+    const sum = (t, v) => t + v;
+    return {
+      sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+      sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+    };
+  }
+
   render() {
     const { handleSubmit, readOnly, credits, debts } = this.props;
-
+    const { sumOfCredits, sumOfDebts } = this.calculateSummary();
     return (
       <form role="form" onSubmit={handleSubmit}>
         <div className="box-body">
@@ -30,6 +39,7 @@ class BillingCycleForm extends Component {
             placeholder="Informe o mês"
             type="number"
           />
+
           <Field
             name="year"
             component={LabelAndInput}
@@ -39,6 +49,7 @@ class BillingCycleForm extends Component {
             placeholder="Informe o ano"
             type="number"
           />
+          <Summary credit={sumOfCredits} debt={sumOfDebts} />
           <ItemList
             cols="12 6"
             list={credits}
