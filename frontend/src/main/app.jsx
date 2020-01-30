bca1d6ffd9802dab5c83bf4099c1d33752194873
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-01-30 00:13:24 -0300

    edit app component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,19 +1,16 @@
-import '../common/template/dependencies'
-import React from 'react'
+import React from "react";
 
-import Header from '../common/template/header'
-import SideBar from '../common/template/sideBar'
-import Footer from '../common/template/footer'
-import Messages from '../common/msg/messages'
+import Header from "../common/template/header";
+import SideBar from "../common/template/sideBar";
+import Footer from "../common/template/footer";
+import Messages from "../common/msg/messages";
 
 export default props => (
-    <div className='wrapper'>
-        <Header />
-        <SideBar />
-        <div className='content-wrapper'> 
-            {props.children}
-        </div>
-        <Footer />
-        <Messages />
-    </div>
-)
+  <div className="wrapper">
+    <Header />
+    <SideBar />
+    <div className="content-wrapper">{props.children}</div>
+    <Footer />
+    <Messages />
+  </div>
+);
